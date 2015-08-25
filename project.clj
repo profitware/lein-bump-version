06a910c3bc73aeb0115d6a86613794d40b9eed83
Michael Drogalis <mjd3089@rit.edu>
2015-08-25 17:53:30 +0000

    Signing key.

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[rewrite-clj "0.4.12"]]
+  :signing {:gpg-key "A4D5C342"}
   :eval-in-leiningen true)
